89d6590723ccc068e84044f79c7d26d8edf5ec11
QA-Automation-Testing-Demo <122603762+QA-Automation-Testing-Demo@users.noreply.github.com>
2025-06-12 15:35:35 +0300

    Update SeleniumTestsBase.cs

--- Eventures.WebApp.SeleniumTests/SeleniumTestsBase.cs
+++ Eventures.WebApp.SeleniumTests/SeleniumTestsBase.cs
@@ -19,23 +19,32 @@ namespace Eventures.WebApp.SeleniumTests
         protected string password = "pass" + DateTime.Now.Ticks.ToString().Substring(10);
 
         [OneTimeSetUp]
-        public void OneTimeSetupBase()
-        {
-            // Run the Web app in a local Web server
-            this.testDb = new TestDb();
-            this.testEventuresApp = new TestEventuresApp<Startup>(
-                testDb, "../../../../Eventures.WebApp");
-            this.baseUrl = this.testEventuresApp.ServerUri;
+public void OneTimeSetupBase()
+{
+    // Run the Web app in a local Web server
+    this.testDb = new TestDb();
+    this.testEventuresApp = new TestEventuresApp<Startup>(
+        testDb, "../../../../Eventures.WebApp");
+    this.baseUrl = this.testEventuresApp.ServerUri;
 
-            // Setup the ChromeDriver
-            var chromeOptions = new ChromeOptions();
-            //if (!Debugger.IsAttached)
-                //chromeOptions.AddArguments("headless");
-            this.driver = new ChromeDriver(chromeOptions);
+    // Setup the ChromeDriver
+    var chromeOptions = new ChromeOptions();
 
-            // Set an implicit wait for the UI interaction
-            this.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-        }
+    //  Add headless and temp profile for GitHub Actions
+    chromeOptions.AddArgument("--headless=new");
+    chromeOptions.AddArgument("--no-sandbox");
+    chromeOptions.AddArgument("--disable-dev-shm-usage");
+
+    // Create a unique temp directory for user profile
+    var tempProfileDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+    chromeOptions.AddArguments($"--user-data-dir={tempProfileDir}");
+
+    this.driver = new ChromeDriver(chromeOptions);
+
+    // Set an implicit wait for the UI interaction
+    this.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+}
+
 
         [OneTimeTearDown]
         public void OneTimeTearDownBase()
